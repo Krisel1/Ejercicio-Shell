9a68037cad6d84979899eee6a5ce57a167b0ad56
Krisel1 <kriselur@gmail.com>
2024-05-16 15:05:54 +0200

    EjerciciosParametros

---
+++ Parametros.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Pasar tres parámetros e imprime el que está en la posición 1 y 3
+
+a=1
+b=2
+c=3
+
+echo "El primer parámetro es: $a"  
+echo "El tercer parámetro es: $c"
+
+#forma mas corta
+echo $1 $3
+
+#Completar la siguiente frase:
+#“En el fichero” <aquí va el nombre del fichero> “existen” <aquí va la cantidad de parámetros>
+
+nombreFichero=
+cantidad=
+
+echo "En el fichero” &nombreFichero “existen” &cantidad
